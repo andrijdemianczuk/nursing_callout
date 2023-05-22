52ea57ab3634fb14bcd517772e2b3e7317ddee0c
andrij.demianczuk@databricks.com <andrij.demianczuk@databricks.com>
2023-05-22 05:20:36 +0000

    Finished the lookup file to table conversion object

--- lookups/Init_Lookups.py
+++ lookups/Init_Lookups.py
@@ -1,4 +1,13 @@
 # Databricks notebook source
+# DBTITLE 1,Global Variables
+USER = "andrij.demianczuk@databricks.com"
+CATALOG = "main"
+
+#Definition of lookup titles. DO NOT CHANGE!
+lookups = ['NC_Med_Units', 'NC_Facilities', 'NC_Nurse_Positions']
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC This is an example of a non-iterated read line for a csv file in a repo
 # MAGIC
@@ -22,36 +31,67 @@
 # MAGIC     df = spark.read.format("csv").option("header", True).load("file:/tmp/" + i + ".csv") 
 # MAGIC     display(df)
 # MAGIC ```
+# MAGIC
+# MAGIC Another option is to use an iterator for the csv files inline. This is the method we will be creating as a class / object feature:
+# MAGIC ```python
+# MAGIC # Print the path
+# MAGIC import sys
+# MAGIC import os
+# MAGIC
+# MAGIC print("\n".join(sys.path))
+# MAGIC sys.path.append(os.path.abspath('/Workspace/Repos/andrij.demianczuk@databricks.com/nursing_callout'))
+# MAGIC
+# MAGIC lookups = ['NC_Med_Units', 'NC_Facilities', 'NC_Nurse_Positions']
+# MAGIC
+# MAGIC for i in lookups:
+# MAGIC     df=spark.read.csv(f"file:{os.getcwd()}/" + i + ".csv", header=True) # "file:" prefix and absolute file path are required for PySpark
+# MAGIC     display(df)
+# MAGIC ```
 
 # COMMAND ----------
 
-# Print the path
+# DBTITLE 1,Create the HLS schema if one doesn't already exist
+#NOTE: Users must have WRITE privileges to the catalog in defined above
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {CATALOG}.hls")
+
+# COMMAND ----------
+
+# DBTITLE 1,Iterate and read each lookup CSV and commit it as a delta table
 import sys
 import os
 
-print("\n".join(sys.path))
-sys.path.append(os.path.abspath('/Workspace/Repos/andrij.demianczuk@databricks.com/nursing_callout'))
-
-lookups = ['NC_Med_Units', 'NC_Facilities', 'NC_Nurse_Positions']
-
-for i in lookups:
-    df=spark.read.csv(f"file:{os.getcwd()}/" + i + ".csv", header=True) # "file:" prefix and absolute file path are required for PySpark
-    display(df)
-
-# COMMAND ----------
-
-class blah():
+class NurseLookups():
+    '''
+    We'll need to PyDoc this class
+    '''
     try:
-        def __init__(self, name:str = "John Doe"):
-            self.greet(name)
+        
+        def __init__(self, user:str):
+            '''
+            user is a required input parameter when instancing this object. This defines under which user the current repository lives.
+            '''
+            self.user = user
 
-        def greet(self, name: str) -> str:
-            print("hi, " + name + "!")
-    except:
-        print("Invalid Exceptional Error")
+        def appendPath(self):
+            '''
+            Ensures that the repo path is part of the Python path
+            '''
+            sys.path.append(os.path.abspath('/Workspace/Repos/' + self.user + '/nursing_callout'))
+            #print("\n".join(sys.path)) #un-comment for path debugging
 
-print(blah())
+        
+        def compileLookups(self, lookups:list):
+            '''
+            A simple iterator that reads the input list and builds a set of delta tables off of them. This is matched to the file names
+            '''
+            for i in lookups:
+                df=spark.read.csv(f"file:{os.getcwd()}/" + i + ".csv", header=True)
+                df.write.format('delta').option("mergeSchema", "true").mode('overwrite').saveAsTable(f"{CATALOG}.hls.l_" + i.lower())
 
-# COMMAND ----------
 
+    except Exception as err:
+        print(err)
 
+n = NurseLookups(user=USER)
+n.appendPath()
+n.compileLookups(lookups=lookups)
